52f31456904e8a1e311552e5e4e8acb5410a80f1
Anish Gyawali <anish.gyawali@selu.edum>
2020-12-27 11:45:14 -0600

    Connection to MongoDB with manual objects

--- MongoDBApp/MongoDB/Program.cs
+++ MongoDBApp/MongoDB/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
 
 namespace MongoDB
 {
@@ -6,7 +8,31 @@ namespace MongoDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MongoCRUD db = new MongoCRUD("AddressBook");
+            db.InsertRecord("Users", new PersonModel { Firstname = "Anish", Lastname = "Gyawali" });
+            Console.ReadLine();
         }
+    }
+    public class PersonModel
+    {
+        [BsonId]  //_Id 
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+    public class MongoCRUD
+    {
+        private IMongoDatabase db;
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+                db = client.GetDatabase(database);
+        }
+    
+    public void InsertRecord<T>(string table, T record)
+    {
+        var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+    }
     }
 }
